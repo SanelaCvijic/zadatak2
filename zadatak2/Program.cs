1c57ed7a43617f6ae6603616250a9b5d6f8dec4d
Sanela Cvijic <ssurucic@yahoo.com>
2020-05-02 20:23:13 +0200

    Add project files.

---
+++ zadatak2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operacija;
+            double broj1;
+            double broj2;
+            double pom;
+
+
+            Console.Write("Unesite operaciju(+,-,*,/)>");
+            operacija = Console.ReadLine();
+            Console.Write("Unesite prvi broj>");
+            broj1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Unesite drugi broj>");
+            broj2 = Convert.ToDouble(Console.ReadLine());
+
+            if (((operacija == "-") || (operacija == "/")) & (broj2 > broj1))
+            {
+                pom = broj1;
+                broj1 = broj2;
+                broj2 = pom;
+            }
+
+            double rezultat = Zadatak2.Op2Numb(operacija, broj1, broj2);
+            Console.WriteLine("Rezultat operacije {0} {1}{2}={3}", broj1, operacija, broj2, rezultat);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ zadatak2/Zadatak2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak2
+{
+    class Zadatak2
+    {
+        public static double Op2Numb(string operacija, double broj1, double broj2)
+        {
+
+
+            switch (operacija)
+            {
+                case "+":
+                    return Add(broj1, broj2);
+                case "-":
+                    return Substract(broj1, broj2);
+
+                case "*":
+                    return Multply(broj1, broj2);
+
+                case "/":
+                    return Divide(broj1, broj2);
+                default:
+                    return 0;
+
+            }
+
+        }
+        public static double Add(double broj1, double broj2)
+        {
+            return broj1 + broj2;
+        }
+
+        public static double Substract(double broj1, double broj2)
+        {
+            if (broj2 > broj1)
+            {
+                return broj2 - broj1;
+            }
+            else
+            {
+                return broj1 - broj2;
+            }
+
+        }
+
+        public static double Multply(double broj1, double broj2)
+        {
+            return broj1 * broj2;
+        }
+        public static double Divide(double broj1, double broj2)
+        {
+            if (broj2 > broj1)
+            {
+                return broj2 / broj1;
+            }
+            else
+            {
+                return broj1 / broj2;
+            }
+
+        }
+
+    }
+}
